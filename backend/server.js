7151ab4e6715475c8433f195c2b1a90b4dc607b9
Duc Le <ducleanh2005@gmail.com>
2025-03-13 10:13:09 -0500

    Added mongoDB and cloudinary

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDb from "./config/mongodb.js";
 
 // App Config
 const app = express();
 const port = process.env.port || 4000;
+connectDb()
 
 // Middlewares
 app.use(express.json());
 app.use(cors()); // front end can connect to backend from any ip
 
-// Api endpointsw
+// Api endpoints
 app.get("/", (req, res) => {
 	res.send("API Working");
 });
