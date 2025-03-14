220375dcf24a8aec6c5d043e05e79c74a21acc5b
Duc Le <ducleanh2005@gmail.com>
2025-03-14 14:57:06 -0500

    Added login, register, and admin register functions; added mongodb, cloudinary, jwt, bcrypt, validator; created user model in mongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,22 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDb from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 
 // App Config
 const app = express();
 const port = process.env.port || 4000;
 connectDb()
+connectCloudinary();
 
 // Middlewares
 app.use(express.json());
 app.use(cors()); // front end can connect to backend from any ip
 
 // Api endpoints
+app.use('/api/user', userRouter);
+
 app.get("/", (req, res) => {
 	res.send("API Working");
 });
